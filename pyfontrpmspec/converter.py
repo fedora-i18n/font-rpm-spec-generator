3f8532919f1371a444f99bdd03e5c5050ac21c90
Akira TAGOH <akira@tagoh.org>
2022-12-20 00:40:34 +0900

    Refactoring and add docstring

--- pyfontrpmspec/converter.py
+++ pyfontrpmspec/converter.py
@@ -16,12 +16,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Module to convert a RPM spec file."""
 
 import argparse
 import shutil
 import subprocess
 import sys
-import _debugpath
+import _debugpath  # noqa: F401
 from pyrpm.spec import Spec
 from pyfontrpmspec import font_reader as fr
 from pyfontrpmspec.messages import Message as m
@@ -31,6 +32,20 @@ from pyfontrpmspec.package import Package
 
 
 def old2new(specfile, args):
+    """
+    Convert `specfile` to new one against Fedora fonts Packaging Guidelines.
+
+    Parameters
+    ----------
+    specfile: str
+              Filename to the targeted RPM spec file
+    args:     dict
+              All the parameters required to convert RPM spec file.
+
+    Returns
+    -------
+    New RPM spec file in string
+    """
     if not shutil.which('rpmspec'):
         raise AttributeError(m().error('rpmspec is not installed'))
     origspec = Spec.from_file(specfile)
@@ -57,135 +72,95 @@ def old2new(specfile, args):
         exdata['setup'] = '-c'
     else:
         exdata['setup'] = '-n {}'.format(exdata['root'])
+    families = []
+    fontconfig = []
+    for k, v in fr.group(exdata['fontinfo']).items():
+        if 'fontmap' in exdata and k in exdata['fontmap']:
+            k = exdata['fontmap'][k]
+        summary = None
+        description = None
+        for p in spec.packages:
+            if Package.is_targeted_package(p.name, exdata['foundry'], k):
+                if p.name == spec.name:
+                    summary = spec.summary
+                    description = spec.description
+                else:
+                    summary = p.summary
+                    description = p.description
+        if not summary:
+            m([': ']).info(k).warning(
+                ('Unable to guess the existing package name. '
+                 'some information may be missing in the spec file')).out()
+        if k not in exdata['fontconfig']:
+            m([': ']).info(k).warning('No fontconfig file')
+        info = {
+            'family': k,
+            'summary': summary,
+            'fonts': ' '.join([vv['file'] for vv in v]),
+            'exfonts': '%{nil}',
+            'conf':
+            len(families) + 10 if k in exdata['fontconfig'] else '%{nil}',
+            'exconf': '%{nil}',
+            'description': description,
+        }
+        families.append(info)
+        if k in exdata['fontconfig']:
+            fontconfig.append(exdata['fontconfig'][k].name)
+    data = {
+        'version':
+        spec.version,
+        'release':
+        origspec.release,
+        'url':
+        spec.url,
+        'origsource':
+        origspec.sources[0],
+        'source':
+        spec.sources[0],
+        'copy_source':
+        not exdata['archive'],
+        'exsources':
+        exdata['sources'],
+        'nsources':
+        exdata['nsources'],
+        'patches':
+        spec.patches,
+        'license':
+        spec.license,
+        'license_file':
+        ' '.join([s.name for s in exdata['licenses']]),
+        'docs':
+        ' '.join([s.name
+                  for s in exdata['docs']]) if 'docs' in exdata else '%{nil}',
+        'foundry':
+        exdata['foundry'],
+        'fonts':
+        families,
+        'fontconfig':
+        fontconfig,
+        'setup':
+        exdata['setup'],
+        'changelog':
+        origspec.changelog.rstrip(),
+    }
     if len(spec.packages) == 1:
-        fontinfo = exdata['fontinfo'][list(exdata['fontinfo'].keys())[0]]
-        family = fontinfo['family']
         if len(exdata['fontconfig']) > 1:
             raise TypeError(m().error('Multiple fontconfig files detected'))
-        if family not in exdata['fontconfig']:
-            raise TypeError(
-                m().error('No fontconfig file for').message(family))
-        # single template
-        data = {
-            'version':
-            spec.version,
-            'release':
-            origspec.release,
-            'url':
-            spec.url,
-            'origsource':
-            origspec.sources[0],
-            'source':
-            spec.sources[0],
-            'copy_source':
-            not exdata['archive'],
-            'exsources':
-            exdata['sources'],
-            'nsources':
-            exdata['nsources'],
-            'patches':
-            spec.patches,
-            'fontconfig':
-            exdata['fontconfig'][family].name,
-            'license':
-            spec.license,
-            'license_file':
-            ' '.join([s.name for s in exdata['licenses']]),
-            'docs':
-            ' '.join([s.name for s in exdata['docs']])
-            if 'docs' in exdata else '%{nil}',
-            'fonts':
-            ' '.join([s.name for s in exdata['fonts']]),
-            'foundry':
-            exdata['foundry'],
-            'family':
-            family,
-            'summary':
-            spec.summary,
-            'description':
-            spec.description.rstrip(),
-            'setup':
-            exdata['setup'],
-            'changelog':
-            origspec.changelog.rstrip(),
-        }
-    else:
-        families = []
-        fontconfig = []
-        for k, v in fr.group(exdata['fontinfo']).items():
-            if 'fontmap' in exdata and k in exdata['fontmap']:
-                k = exdata['fontmap'][k]
-            summary = None
-            description = None
-            for p in spec.packages:
-                if Package.is_targeted_package(p.name, k):
-                    if p.name == spec.name:
-                        summary = spec.summary
-                        description = spec.description
-                    else:
-                        summary = p.summary
-                        description = p.description
-            if not summary:
-                m([': ']).info(k).warning(
-                    'Unable to guess the existing package name. some information may be missing in the spec file'
-                ).out()
-            if k not in exdata['fontconfig']:
-                m([': ']).info(k).warning('No fontconfig file')
-            info = {
-                'family': k,
-                'summary': summary,
-                'fonts': ' '.join([vv['file'] for vv in v]),
-                'exfonts': '%{nil}',
-                'conf':
-                len(families) + 10 if k in exdata['fontconfig'] else '%{nil}',
-                'exconf': '%{nil}',
-                'description': description,
-            }
-            families.append(info)
-            if k in exdata['fontconfig']:
-                fontconfig.append(exdata['fontconfig'][k].name)
-        data = {
-            'version':
-            spec.version,
-            'release':
-            origspec.release,
-            'url':
-            spec.url,
-            'origsource':
-            origspec.sources[0],
-            'source':
-            spec.sources[0],
-            'copy_source':
-            not exdata['archive'],
-            'exsources':
-            exdata['sources'],
-            'nsources':
-            exdata['nsources'],
-            'patches':
-            spec.patches,
-            'license':
-            spec.license,
-            'license_file':
-            ' '.join([s.name for s in exdata['licenses']]),
-            'docs':
-            ' '.join([s.name for s in exdata['docs']])
-            if 'docs' in exdata else '%{nil}',
-            'foundry':
-            exdata['foundry'],
-            'fonts':
-            families,
-            'fontconfig':
-            fontconfig,
-            'setup':
-            exdata['setup'],
-            'changelog':
-            origspec.changelog.rstrip(),
-        }
+        data['family'] = families[0]['family']
+        if data['family'] not in exdata['fontconfig']:
+            raise TypeError(m().error('No fontconfig file for').message(
+                data['family']))
+        data['summary'] = families[0]['summary']
+        data['description'] = families[0]['description']
+        data['fontconfig'] = '%{nil}' if len(
+            data['fontconfig']) == 0 else data['fontconfig'][0]
+        data['fonts'] = families[0]['fonts']
 
     return template.get(len(spec.packages), data)
 
 
 def main():
+    """Endpoint function to convert a RPM spec file from given parameters."""
     parser = argparse.ArgumentParser(
         description='Fonts RPM spec file converter against guidelines',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -215,8 +190,9 @@ def main():
             m().message('Proposed spec filename is').info(r + '.spec').out()
 
     m([': ', ' ']).warning('Note').message(
-        'You have to review the result. this doesn\'t guarantee that the generated spec file can be necessarily built properly.'
-    ).out()
+        ('You have to review the result. '
+         'this doesn\'t guarantee that the generated spec file'
+         ' can be necessarily built properly.')).out()
 
 
 if __name__ == '__main__':
--- pyfontrpmspec/package.py
+++ pyfontrpmspec/package.py
@@ -16,19 +16,34 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Module to deal with packages."""
 
 import re
 import shutil
 import subprocess
-import _debugpath
+import _debugpath  # noqa: F401
 from pyfontrpmspec.messages import Message as m
 from pyrpm.spec import Spec
 
 
 class Package:
+    """Package related class."""
 
     @staticmethod
     def source_name(src):
+        """
+        Get a Source package name for `src`.
+
+        Parameters
+        ----------
+        src: str
+             Spec filename
+
+        Returns
+        -------
+        str
+            Source package name
+        """
         if src.endswith('.spec'):
             if not shutil.which('rpmspec'):
                 raise AttributeError(m().error('rpmspec is not installed'))
@@ -40,8 +55,128 @@ class Package:
                 m().error('Unsupported filetype:').message(src))
 
     @staticmethod
-    def is_targeted_package(pkg, family):
-        return re.fullmatch(r'{}'.format(family.replace(' ', '').lower()),
-                            pkg.replace('-fonts',
-                                        '').replace('-',
-                                                    '').lower()) is not None
+    def build_package_name(foundry, family):
+        """
+        Build a package name for foundry and family.
+
+        Parameters
+        ----------
+        foundry: str
+                 Foundry name
+        family:  str
+                 Family name
+
+        Returns
+        -------
+        str
+            Package name in string
+        """
+        return str(
+            FamilyString(foundry + ' ' + re.sub(r'^{}'.format(
+                foundry), '', family)).normalize()) + '-fonts'
+
+    @staticmethod
+    def is_targeted_package(pkg, foundry, family):
+        """
+        Check `pkg` is a package name for `foundry` and `family`.
+
+        Returns
+        -------
+        bool
+             True if `pkg` is a package name for `foundry` and `family`.
+             Otherwise False.
+        """
+        return pkg == Package.build_package_name(foundry, family)
+
+
+class FamilyString:
+    """
+    Wrapper class to deal with a string of font family name.
+
+    Parameters
+    ----------
+    string: str
+            A family name.
+    """
+
+    def __init__(self, string):
+        """Initialize a FamilyString.
+
+        Parameters
+        ----------
+        string: str
+                A family name in string
+        """
+        self.__string = string
+
+    def __normalize(self, name):
+        norm = re.sub(r'-$', '', re.sub(r'^-', '',
+                                        re.sub(r'[\W_]+', '-', name))).lower()
+        return norm
+
+    def __normalize_mutable(self):
+        self.__string = self.__normalize(self.__string)
+        return self
+
+    def __dropsuffix(self, slist):
+        n = self.__string
+        for s in slist:
+            n, c = re.subn(r'-{}$'.format(self.__normalize(s)), '', n)
+            if c == 1:
+                break
+        self.__string = n
+        return self
+
+    def __dropduplicate(self):
+        tokens = ['font', 'fonts']
+        s = ''
+        for t in self.__string.split('-'):
+            if t not in tokens:
+                s = s + '-' + t
+                tokens.append(t)
+        self.__string = re.sub(r'^-', '', s)
+        return self
+
+    def normalize(self):
+        """
+        Normalize a family name in string.
+
+        this is required process to make it for package name.
+
+        Returns
+        -------
+        str
+            a normalized string
+        """
+        return self.__normalize_mutable().__dropsuffix(
+            ['normal', 'book', 'regular', 'upright']).__dropsuffix([
+                'italic', 'ita', 'ital', 'cursive', 'kursiv', 'oblique',
+                'inclined', 'backslanted', 'backslant', 'slanted'
+            ]).__dropsuffix([
+                'ultracondensed', 'extra-compressed', 'ext-compressed',
+                'ultra-compressed', 'ultra-condensed', 'extracondensed',
+                'compressed', 'extra-condensed', 'ext-condensed', 'extra-cond',
+                'semicondensed', 'narrow', 'semi-condens', 'semiexpanded',
+                'wide', 'semi-expanded', 'semi-extended', 'extraexpanded',
+                'extra-expanded', 'ext-expanded', 'extra-extended',
+                'ext-extended', 'ultraexpanded', 'ultra-expanded',
+                'ultra-extended', 'condensed', 'cond', 'expanded', 'extended'
+            ]).__dropsuffix([
+                'thin', 'extra-thin', 'ext-thin', 'ultra-thin', 'extralight',
+                'extra-light', 'ext-light', 'ultra-light', 'demibold',
+                'semi-bold', 'demi-bold', 'extrabold', 'extra-bold',
+                'ext-bold', 'ultra-bold', 'extrablack', 'extra-black',
+                'ext-black', 'ultra-black', 'bold', 'thin', 'light', 'medium',
+                'black', 'heavy', 'nord', 'demi', 'ultra'
+            ]).__dropduplicate()
+
+    def __str__(self):
+        """
+        Represent a string held in FamilyString class.
+
+        Returns
+        -------
+        str
+            a string
+        """
+        return self.__string
